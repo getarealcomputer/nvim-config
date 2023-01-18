64dc06f2f5481e1b7547f7460e3f31f6df065a36
getarealcomputer <getarealcomputer@gmail.com>
2023-01-18 07:32:09 +0700

    treesitter fix

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,6 +1,6 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+  return
 end
 
 configs.setup({
@@ -8,13 +8,11 @@ configs.setup({
     "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx",
     "css", "rust", "java", "yaml", "markdown", "markdown_inline"
   }, -- one of "all" or a list of languages
-	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "python", "css" } },
+  highlight = {
+    enable = true, -- false will disable the whole extension
+  },
+  autopairs = {
+    enable = true,
+  },
+  indent = { enable = true, disable = { "python", "css" } },
 })
