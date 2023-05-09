43906bceca582b85618b6e579e88153ef6ad929d
getarealcomputer <getarealcomputer@gmail.com>
2023-05-09 17:25:16 +0700

    adding some plugins, adding solidity lsp

--- lua/getarealcomputer/lazy/plugins/notification.lua
+++ lua/getarealcomputer/lazy/plugins/notification.lua
@@ -1,51 +1,20 @@
 return {
-  -- Better `vim.notify()`
-  {
-    "rcarriga/nvim-notify",
-    enabled = false,
-    keys = {
-      {
-        "<leader>un",
-        function()
-          require("notify").dismiss({ silent = true, pending = true })
-        end,
-        desc = "Delete all Notifications",
-      },
-    },
-    opts = {
-      timeout = 3000,
-      max_height = function()
-        return math.floor(vim.o.lines * 0.75)
-      end,
-      max_width = function()
-        return math.floor(vim.o.columns * 0.75)
-      end,
-    },
-    init = function()
-    --  -- when noice is not enabled, install notify on VeryLazy
-    --  local Util = require("lazyvim.util")
-    --  if not Util.has("noice.nvim") then
-    --    Util.on_very_lazy(function()
-      vim.notify = require("notify")
-    --    end)
-    --  end
-    end,
-  },
-    -- noicer ui
+  -- noicer ui
   {
     "folke/noice.nvim",
     event = "VeryLazy",
+    enabled = true,
     opts = {
       lsp = {
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
         },
       },
       presets = {
         bottom_search = true,
         command_palette = true,
-        long_message_to_split = true,
       },
     },
     -- stylua: ignore
@@ -58,6 +27,6 @@ return {
       { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
     },
   },
-    -- ui components
+  -- ui components
   { "MunifTanjim/nui.nvim", lazy = true },
 }
