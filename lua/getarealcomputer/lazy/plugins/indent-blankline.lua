b1c3f573d13b9ffc673713965719c9fa8227787c
getarealcomputer <getarealcomputer@gmail.com>
2023-12-31 14:28:07 +0900

    indentblankline config
    indentblankline now excluding help and other filetypes

--- lua/getarealcomputer/lazy/plugins/indent-blankline.lua
+++ lua/getarealcomputer/lazy/plugins/indent-blankline.lua
@@ -1,31 +1,41 @@
 return {
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      --char = "│",
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        event = { "BufReadPost", "BufNewFile" },
+        opts = {
+            enabled = true,
+            exclude = {
+                filetypes = {
+                    "help",
+                    "man",
+                    "lspinfo",
+                    "packer",
+                    "checkhealth",
+                    "gitcommit",
+                    "TelescopePrompt",
+                    "TelescopeResults",
+                },
+            },
+        },
+        config = function(_, opts)
+            require("ibl").setup(opts)
+        end,
     },
-    config = function (_, opts)
-      require("ibl").setup({
-
-      })
-    end
-  },
-  {
-    "echasnovski/mini.indentscope",
-    version = false, -- wait till new 0.7.0 release to put it back on semver
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-      draw = {
-        delay = 1
-      },
-      -- symbol = "▏",
-      symbol = "│",
-      options = { try_as_border = true },
+    {
+        "echasnovski/mini.indentscope",
+        version = false, -- wait till new 0.7.0 release to put it back on semver
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            draw = {
+                delay = 1,
+            },
+            -- symbol = "▏",
+            symbol = "│",
+            options = { try_as_border = true },
+        },
+        config = function(_, opts)
+            require("mini.indentscope").setup(opts)
+        end,
     },
-    config = function(_, opts)
-      require("mini.indentscope").setup(opts)
-    end
-  }
 }
