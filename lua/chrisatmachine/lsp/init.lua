13018d6631202e3955136a2b8f5e29199fd7df95
getarealcomputer <getarealcomputer@gmail.com>
2024-01-14 13:54:57 +0900

    Hard Resetting Main Branch
    I think it is time to reset.
    Time to make a new clean commits.

--- lua/chrisatmachine/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "chrisatmachine.lsp.mason"
-require("chrisatmachine.lsp.handlers").setup()
-require "chrisatmachine.lsp.null-ls"
--- lua/user/init.lua
+++
@@ -1,7 +0,0 @@
-require("user.options")
-require("user.plugins")
-require("user.colorscheme")
-require("user.autocommands")
-require("user.lsp")
-require("user.greeter")
-require("user.lualine")
